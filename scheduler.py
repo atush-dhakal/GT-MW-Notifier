95941f50f024ff0062b928f96acca2fb2c14de44
iBaniya <ibaniya98@gmail.com>
2021-01-04 23:16:10 -0500

    Configure for Cloud Scheduler (#5)
    * Add endpoint for cloud scheduler to trigger scraping
    
    * Add email password to config file
    
    * Formatting fixes

--- scheduler.py
+++ scheduler.py
@@ -5,19 +5,20 @@ import jobs_list
 from email_notifier import send_notification
 from mailchimp import OnCampusJobList
 
+
 def run_schedule():
     new_jobs = jobs_list.populate_new_jobs()
-    print(new_jobs)
-    if (len(new_jobs) > 0):
+
+    if len(new_jobs) > 0:
         custom_list = OnCampusJobList()
         members = custom_list.get_email_list()
-        for elem in new_jobs:
-            send_notification(members, elem)
-    print("done!")
+        for job in new_jobs:
+            send_notification(members, job)
 
 
-schedule.every(20).seconds.do(run_schedule)
+if __name__ == "__main__":
+    schedule.every(20).seconds.do(run_schedule)
 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
